35359d040a6c1c7a2e6a6256763c9aa724f4cc9c
vladimir <vavakharitonov2004@gmail.com>
2025-06-23 22:30:56 +0300

    Add cache

--- backend/src/http_client.py
+++ backend/src/http_client.py
@@ -1,4 +1,5 @@
 from aiohttp import ClientSession
+from async_lru import alru_cache
 
 class HttpClient:
     def __init__(self, base_url: str, api_key: str):
@@ -10,11 +11,13 @@ class HttpClient:
         )
 
 class CMCHTTPClient(HttpClient):
+    @alru_cache
     async def get_listings(self):
         async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
             result = await resp.json()
             return result["data"]
 
+    @alru_cache
     async def get_currency(self, currency_id: int):
         async with self._session.get(
                 "/v2/cryptocurrency/quotes/latest",
